cb0a0bbcfc84b8dc9062a2b04e3f227d903a7517
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2020-12-05 21:15:26 +0300

    Shiny app for a farmer with iris data included.

--- shiny_app.R
+++ shiny_app.R
@@ -1,26 +1,68 @@
 # define user interface
+data(iris)
 
-ui <- fluidPage(theme = shinytheme("united"),
+
+library(shiny)
+library(shinythemes)
+
+user_interface <- fluidPage(titlePanel("Kenyan Farmer"),
+                            theme = shinytheme("united"),
                 navbarPage(
-                  # theme = "cerulean",
-                  "My first app",
-                  tabPanel("Navbar1",
+                  "KALRO - TRI Records",
+                  tabPanel("Farmer Details",
                            sidebarPanel(
-                             tags$h3("Input:"),
-                             textInput("txt1", "Given Name:", ""),
+                             tags$h3("Name:"),
+                             textInput("txt4", "Registration Number:"),
+                             textInput("txt1", "Given Name(s):", ""),
                              textInput("txt2", "Surname:", ""),
+                             textInput("txt3", "Age:", ""),
+                             selectInput("slctInput", "Select country you live in",
+                                         choices = c("Kenya", "Uganda", "Tanzania"))
                              
                            ), # sidebarPanel 1
                            mainPanel(
-                             h1("Header 1"),
-                             h4("Output 1"),
+                             h1("Contact Details"),
+                             h4("Full Names"),
                              verbatimTextOutput("txtout"),
+                             textOutput("countryOutput"),
                              
                            ) # mainPanel
                            
                   ), # navbar 1 tabpanel 1
-                  tabPanel("Navbar 2", "I want to leave this empty"),
-                  tabPanel("Navbar 3", "This panel is supposed to be blank")
+                  tabPanel("Coffee Yield Trend",
+                           sidebarPanel(
+                             sliderInput("obs",
+                                         "Coffee Yield (Kgs):",
+                                         min = 1,
+                                         max = 50,
+                                         value = 25,
+                                         step = 5)
+                           ),
+                           mainPanel(
+                             h1("Distribution"),
+                             h3("In Kgs"),
+                           )
+                           ),
+                  tabPanel("Iris data by Species"),
+                  sidebarLayout(
+                    sidebarPanel(
+                      selectInput("var", "1. Select the variable from the iris datset",
+                                  choices = c("Sepal.Length" = 1,
+                                              "Sepal.Width" = 2,
+                                              "Petal.Length" = 3,
+                                              "Petal.Width" = 4),
+                                  selected = 1),
+                      br(),
+                      sliderInput("bins", "2. Select the number of BINs for histogram",
+                                  min = 5, max = 25, value = 15),
+                      br(),
+                      radioButtons("color", "3. Select the color of histogram",
+                                   choices = c("Purple", "Cyan", "Magenta"), selected = "Purple")
+                    ),
+                    mainPanel(
+                      plotOutput("myhist")
+                    )
+                  )
                 ) # navbar page
 ) # fluidpage
 
@@ -30,9 +72,20 @@ ui <- fluidPage(theme = shinytheme("united"),
 server <- function(input, output){
   
   output$txtout <- renderText({
-    paste( input$txt1, input$txt2, sep = " ")
+    paste( input$txt1, input$txt2, input$txt3, input$txt4, sep = " ")
+    
+  })
+  output$countryOutput <- renderText({
+    paste(input$slctInput)
+  })
+  output$myhist <- renderPlot({
+    colm <- as.numeric(input$var)
+    hist(iris[,colm], breaks = seq(0, max(iris[, colm], l = input$bins + 1)),
+                                    col = input$color)
   })
 } # server
 
 # create shiny object
-shinyApp(ui = ui, server = server)
+shinyApp(ui = user_interface, server = server)
+
+
