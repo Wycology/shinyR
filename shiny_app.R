da4ba670f9790720ba08fb7b49dbcf71abea9691
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-02-23 10:29:55 +0300

    Adding cars data.

--- shiny_app.R
+++ shiny_app.R
@@ -6,7 +6,7 @@ library(shinythemes) # For changing the themes of the web application created.
 # Load the necessary data 
 
 data(iris) # iris flower data
-data(mtcars) # cars data which also comes with the base R package
+data(mtcars) # cars data which also come with the base R package
 
 # Define user interface. This is page the users will be interacting with.
 
