fcfc1fea0b57b34d4bbf76e54364718509c1fc14
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-10-06 15:18:50 +0300

    c

--- shiny_app.R
+++ shiny_app.R
@@ -5,7 +5,7 @@
 # Load the packages for building the app
 #----------------------------------------------------
 library(shiny)
-library(shinythemes) # For changing the themes.
+library(shinythemes) # For changing the t
 library(tidyverse)
 
 # Loading two data-sets for the app building 
