926e59cd5e4754fc86abbce4f941412c8d1d0158
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-10-13 22:49:24 +0300

    c

--- shiny_app.R
+++ shiny_app.R
@@ -37,7 +37,7 @@ ui <- fluidPage(
                              
                            ) # mainPanel defini
                            
-                  ), # navigation bar 1 tab
+                  ), # navigation bar 1
                   tabPanel("Tea Yield Trend",
                            sidebarPanel(
                              sliderInput("obs",
