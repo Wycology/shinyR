6da912275fd5a4460f89430e11e1a2ad50fbadb3
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-10-15 21:41:19 +0300

    c

--- basicapp.R
+++ basicapp.R
@@ -4,7 +4,7 @@ library(ggplot2)
 library(Cairo)   # For nicer ggplot2 output when deployed on Linux
 library(DT)
 
-# A modified version of mtcars with some columns removed, some columns
+# A modified version of mtcars with some columns removed, some
 mtc <- mtcars
 mtc$cyl <- factor(mtc$cyl)
 mtc$am  <- factor(mtc$am)
