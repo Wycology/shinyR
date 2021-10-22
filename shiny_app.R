ae76fdaf750ced09755caa345eabfe6b60bc6b66
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-10-22 21:06:08 +0300

    c

--- shiny_app.R
+++ shiny_app.R
@@ -8,7 +8,7 @@ library(shiny)
 library(shinythemes) 
 library(tidyverse)
 
-data(iris) # iris data 
+data(iris) # iris
 data(mtcars) # mtcars data with 32 observations and 11 variables
 
 # Defining user interface, the so called ui. 
