2632b7262ae1c001fcde448aa6c16ac397bec398
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-10-27 09:42:05 +0300

    c

--- shiny_app.R
+++ shiny_app.R
@@ -2,7 +2,7 @@
 
 # shinyApp in R, last edited 26th Oct 2021 by the author
 
-# Load the packages for building the app
+# Load the packages for building the ap
 #----------------------------------------------------
 library(shiny)
 library(shinythemes) 
