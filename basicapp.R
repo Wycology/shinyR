40f05a4fb1e7749cf6e0cb6a7fafa3da52f1595e
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2023-10-20 22:14:28 +0200

    Update basicapp.R

--- basicapp.R
+++ basicapp.R
@@ -13,7 +13,7 @@ mtc$gear <- NULL
 mtc$drat <- NULL
 mtc$carb <- NULL
 
-mtc$date <- Sys.Date() + seq_len(nrow(mtc))# 
+mtc$date <- Sys.Date() + seq_len(nrow(mtc)) # 
 mtc$datetime <- Sys.time() + 60 * seq_len(nrow(mtc))
 
 # Data set with points on a grid
