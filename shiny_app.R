79503eac3d9f9b3d9d853cf3c47272f6e396a2ff
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2023-11-20 21:30:43 +0300

    Update shiny_app.R

--- shiny_app.R
+++ shiny_app.R
@@ -9,7 +9,7 @@ pacman::p_load(shiny, shinythemes, tidyverse)
 data(iris)
 data(mtcars)
 
-# Defining user interface, the so called ui. 
+# Defining tuser interface, the so called ui. 
 
 ui <- fluidPage(
   titlePanel("Kenyan Tea Farmers Details"),
